feb7f25ca45b44615a2c73dc72d4ebdd65ac740f
kelseyblanchard <73808546+kelseyblanchard@users.noreply.github.com>
2023-11-19 11:32:20 -0500

    Updated Counter.test.js
    Added full coverage testing functionality for Counter.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,36 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const headerMessage = screen.getByText(/Counter/i);
+  expect(headerMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countElement = screen.getByTestId("count");
+  expect(countElement).toBeInTheDocument();
+
+  expect(countElement.textContent).toBe("0");
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countElement = screen.getByTestId("count");
+  const decrementButton = screen.getByText("+");
+
+  fireEvent.click(decrementButton);
+
+  expect(countElement.textContent).toBe("1");
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking - decrements the count', async () => {
+  const countElement = screen.getByTestId("count");
+  const decrementButton = screen.getByText("-");
+
+  fireEvent.click(decrementButton);
+
+  expect(countElement.textContent).toBe("-1");
 });
